0497de97b41780699467bf222830826b7190d7d5
areebey <areebrafiq125@gmail.com>
2024-08-31 18:40:56 -0700

    Complete footer and working on popular doctors section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Hero from "./_components/Hero";
 import CategorySearch from "./_components/CategorySearch";
 import ImageGallery from "./_components/ImageGallery";
+import DoctorList from "./_components/Doctorlist";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
      <Hero />
      <CategorySearch/>
      <ImageGallery/>
+     <DoctorList/>
     </>
   );
 }
