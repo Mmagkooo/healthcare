a103aa42e47f3564fa695d416cbbed5d8e35e4a0
GhulamHaider79 <127304370+GhulamHaider79@users.noreply.github.com>
2024-09-05 01:50:50 +0500

    fixed input error

--- src/app/_components/CategorySearch.jsx
+++ src/app/_components/CategorySearch.jsx
@@ -1,7 +1,7 @@
 // src/components/CategorySearch.js
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import Input from '@/components/ui/Input'; 
+import Input from '@/components/ui/input'; 
 import { Search } from 'lucide-react';
 
 function CategorySearch() {
@@ -11,7 +11,7 @@ function CategorySearch() {
         Search <span className='text-primary'>Doctor</span>
       </h2>
       <p className='text-base sm:text-lg md:text-xl text-gray-500 text-center'>
-        Search Your Doctor
+        Search Your Doctors
       </p>
       <div className="mt-4 flex flex-col sm:flex-row w-full max-w-md sm:max-w-lg lg:max-w-xl items-center">
         <Input type="text" placeholder="Search..." className="flex-1 mb-2 sm:mb-0 sm:mr-2" />
